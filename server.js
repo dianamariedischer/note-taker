411dc1f651ee3a32dd8828ca7088d4611da23f24
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-06 20:45:50 -0500

    edited commentary and refactored

--- server.js
+++ server.js
@@ -1,63 +1,63 @@
-// Import express package
+// import express package
 const express = require('express');
 
-// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+// importing 'path' - a node.js package that resolves the path of files on the server
 const path = require('path');
+// import file system to write over the notes db.json file
 const fs = require('fs');
+// notes variable that points to the location of the db.json file
+// allows easy edits to the array containing note objects
 const notes = require('./db/db.json');
 
-// Specify on which port the Express.js server will run
+// specify which port Express.js server will run on
 const PORT = 3001;
 
-// Initialize our app variable by setting it to the value of express()
+// app variable initialized and set to express()
 const app = express();
 
 // take in json type data
 app.use(express.json());
 
-// Static middleware pointing to the public folder
-// files that are publicly available
+// static middleware that points to the public folder
 app.use(express.static('public'));
 
-// GET route to get all of the notes
+// GET route that sends the index html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+// GET route that sends the html for the notes page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 // GET request handler for note db
 app.get('/api/notes', (req, res) => {
     console.info(`GET /api/notes`);
-    // status 200 is a good/ok response for a successful get
+    // status 200 is a successful get, returns the content of notes in json
     res.status(200).json(notes);
 });
 
-// GET request for notes
-app.get('/notes', (req, res) => {
-    // send notes
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
-
+// POST request to add a note
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
+    // Logs that POST request was receieved
     console.info(`${req.method} request received to add a note`);
   
+    // new note id to put it at the end of the array
     const id = notes.length + 1;
-    // Destructuring assignment for the items in req.body
+    // Destructuring for the items in req.body / the note being added
     const { title, text } = req.body;
     
-    // If all the required properties are present
-    // validating if the user entered all fields
+    // make sure all the required properties are present
     if (title && text) {
-        // Variable for the object we will save
+        // create new note object
         const newNote = {
             id,
             title,
             text,
         };
 
-        //const notesArray = JSON.parse(notes);
+        // add the new note to the end of the notes array file
         notes.push(newNote);
-    
+
         const notesString = JSON.stringify(notes);
-        // Write the stringified array with the added note to the db file
+        // Write the new stringified array to the db file
         fs.writeFile(`./db/db.json`, notesString, (err) =>
             err
             ? console.error(err)
@@ -66,35 +66,38 @@ app.post('/api/notes', (req, res) => {
             )
         );
 
-
-        // res.json() returns data including a status message indicating the success of the request along with the newly created review data.
-        // 201 is a success response for something created, and res.json sends the success message back to the client
+        // send status of a successful post and the content of the note added
         res.status(201).json(newNote);
     } else {
-        // the purpose of the else statement is to allow a way to throw an error if either the product, review, or username is not present.
-        // 500 response for something is wrong, and we send back a non-specific error to let the client there was a problem in the data in some way.
-        res.status(500).json('Error in posting note');
+        // log an error if required properties are not present
+        res.status(500).json("Error in posting note, make sure you've input all necessary data");
     }
 });
 
 app.delete('/api/notes/:id', (req, res) => {
+    // check to make sure there is a note item with that id
+    if(notes[req.params.id - 1]){
+        notes.splice(req.params.id - 1, 1);
 
-    notes.splice(req.params.id - 1, 1);
+        for (i = 0; i < notes.length; i++) {
+            notes[i].id = i + 1;
+        }
 
-    for (i = 0; i < notes.length; i++) {
-        notes[i].id = i + 1;
+        const notesString = JSON.stringify(notes);
+        // Write the new stringified array to the db file
+        fs.writeFile(`./db/db.json`, notesString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(
+                    `Note ${req.params.id} has been deleted.`
+                )
+        );
+
+        res.status(204).json(req.params.id);
+    } else {
+        res.status(500).json("No note found with the selected id");
     }
-
-    const notesString = JSON.stringify(notes);
-    // Write the new stringified array to the db file
-    fs.writeFile(`./db/db.json`, notesString, (err) =>
-        err
-        ? console.error(err)
-        : console.log(
-            `The note has been deleted.`
-        )
-    );
-    res.status(200).json(req.params.id);
+    
 });
 
 // listen() method is responsible for listening for incoming connections on the specified port
